a9e48b806bb9eafc505f13d7d91854a26b236ccb
Tarun Jaikishan <tarunjaikishanpk@gmail.com>
2025-03-16 20:08:44 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import TestPage from "./pages/TestPage";
 
 function App() {
   return (
-    <>
+    <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/test" element={<TestPage />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
